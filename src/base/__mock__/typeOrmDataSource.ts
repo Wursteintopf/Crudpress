79965ce64abc31f0eb4982edc6b65e034513a9f4
Wursteintopf <markus@vogelvlug.de>
2022-12-01 19:43:02 +0100

    Add tests

---
+++ src/base/__mock__/typeOrmDataSource.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { TestModel } from './TestModel'
+
+export const appDataSource = new DataSource({
+  type: 'better-sqlite3',
+  database: ':memory:',
+  synchronize: true,
+  logging: false,
+  entities: [TestModel],
+  migrations: [],
+  subscribers: [],
+})
